785e462f94aab9f0cdfd19cea6f296c62e9da68e
Matthieu Coudron <mcoudron@hotmail.com>
2020-09-02 17:44:25 +0200

    added poetry2nix override but still fails

--- contrib/shell.nix
+++ contrib/shell.nix
@@ -1,7 +1,20 @@
-with import <nixpkgs> {};
-
 let
-  mptcpanalyzer = callPackage ../default.nix {};
+
+  # pkgs = import ./nixpkgs.nix {
+  pkgs = import <nixpkgs> {
+    overlays = [
+      # latest version of poetry and poetry2nix
+      (import "${poetry2nixSrc}/overlay.nix")
+    ];
+  };
+
+  # temporary overlay (remove on next nixpkgs bump)
+  poetry2nixSrc = (fetchTarball {
+    url = "https://github.com/teto/poetry2nix/archive/44f5092c6373ebb0a53bd0e3828ebdb58ab1612b.tar.gz";
+    sha256 = "09728x7snn6z8qaqqirs2gc9g8rxrvamiw37qhwp1yvmp9q5b771";
+  });
+
+  mptcpanalyzer = pkgs.callPackage ../default.nix {};
 
   prog = (mptcpanalyzer.override({
     # inherit pandas;
@@ -12,15 +25,15 @@ let
     version = "0.3.3-dev";
     nativeBuildInputs = (oa.nativeBuildInputs or []) ++ [
       # to publish on pypi
-      pkgs.python3Packages.twine
-      python-language-server
+      # pkgs.python3Packages.twine
+      # pkgs.python-language-server
 
     ];
     propagatedBuildInputs  = (oa.propagatedBuildInputs  or []) ++ [
       # my_nvim.config.python3Env
 
       # temporary addition to work with mpls
-      openssl
+      # pks.openssl
     ];
 
     src = ../.;
@@ -33,8 +46,6 @@ let
       echo "SOURCE_DATE_EPOCH: $SOURCE_DATE_EPOCH"
       export PYTHONPATH="$tmp_path/lib/python3.7/site-packages:$PYTHONPATH"
       python -m pip install -e . --prefix $tmp_path >&2
-      export PATH="${my_nvim}/bin:$PATH"
-      echo "importing a custom nvim ${my_nvim}"
 
       alias m=mptcpanalyzer
     '';
@@ -43,10 +54,7 @@ let
 
 
   # https://www.reddit.com/r/neovim/comments/b1zm7h/how_to_setup_microsofts_python_lsp_in_linuxubuntu/
-  my_nvim = genNeovim  [ mptcpanalyzer ] {
-    # ms-python.python
-    # coc-python
-
+  # my_nvim = genNeovim  [ mptcpanalyzer ] {
     # plugins = [ vimPlugins.coc-python ];
     # configure = {
     #     packages.myVimPackage = {
@@ -54,17 +62,17 @@ let
     #       # loaded on launch
     #       start = startPlugins;
     # };
+    # configure.packages.myVimPackage.start = [];
+    # # extraPython3Packages = ps: with ps;  [ python-language-server ];
+  # };
 
-    configure.packages.myVimPackage.start = [ vimPlugins.coc-python ];
-    # extraPython3Packages = ps: with ps;  [ python-language-server ];
-  };
-
-  prog2 = poetry2nix.mkPoetryEnv {
+  # prog2 = pkgs.poetry2nix.mkPoetryEnv {
+  prog2 = pkgs.poetry2nix.mkPoetryApplication {
     # because shell.nix is in contrib/ folder
     projectDir = ../.;
-    editablePackageSources = {
-      mptcpanalyzer = ./.;
-    };
+    # editablePackageSources = {
+    #   mptcpanalyzer = ./.;
+    # };
   };
 in
 # TODO generate our own nvim
